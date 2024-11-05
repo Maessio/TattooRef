1f17c4169f25fa795ee4db05b5e537b5284f6840
Maessio <maessiosousa@gmail.com>
2024-11-04 22:34:11 -0300

    refactor: updating base configs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component, inject } from '@angular/core';
-import { FooterComponent } from "../../components/footer/footercomponent";
 import { CarouselComponent } from "../../components/carousel/carousel.component";
 import { ApiService } from '../../services/api.service';
 import { FormsModule } from '@angular/forms';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [FooterComponent, CarouselComponent, FormsModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   private service = inject(ApiService);
